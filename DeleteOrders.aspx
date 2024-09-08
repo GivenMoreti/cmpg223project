ed93a1aa875863641bac61286d2c97a17ab0305c
Akhals <1my2info@gmail.com>
2024-09-08 21:19:46 +0200

    added order files
    required order files

---
+++ DeleteOrders.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DeleteOrders.aspx.cs" Inherits="InventoryManagementSystemCMPG223.DeleteOrders" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Delete Orders</title>
+     <link rel="stylesheet" href="ProductStyles.css" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <table style="width: 100%;">
+                <tr>
+                    <td style="width: 20%;">&nbsp;</td>
+                    <td id="Delete order ">Delete An Order</td>
+                    <td style="width: 20%;">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="width: 20%;">&nbsp;</td>
+                    <td class="content">
+                        <asp:TextBox ID="OrderIdTB" runat="server" placeholder="Enter Order ID"></asp:TextBox><br />
+                        <asp:Button ID="DeleteOrderBtn" runat="server" Text="Delete Order" OnClick="DeleteOrderBtn_Click" />
+                        <br />
+
+                        <asp:Label ID="FeedbackLbl2" runat="server" Text=""></asp:Label><br />
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td>
+
+
+                        <asp:HyperLink ID="HyperLink1" NavigateUrl="~/Orders.aspx" runat="server">See Orders</asp:HyperLink>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DeleteOrders.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InventoryManagementSystemCMPG223
+{
+    public partial class DeleteOrders : System.Web.UI.Page
+    {
+        int OrderIdTB2;
+        string FeedbackLbl;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        // DEPENDENCIES
+        string ConnString = @"Data Source=TAHERA\SQLEXPRESS;Initial Catalog=InventoryManagementSystemDb;Integrated Security=True;TrustServerCertificate=True";
+        SqlConnection conn;
+        SqlDataAdapter adapter;
+        SqlCommand cmd;
+
+        protected void DeleteOrderBtn_Click(object sender, EventArgs e)
+        {
+            if (IsValidId())
+            {
+                int orderId;
+                if (int.TryParse(OrderIdTB2.ToString(), out orderId))
+                {
+                    string query = "Delete from OrdersTable where OrderId=@orderId";
+                    DeleteOrder(query, orderId);
+                }
+                else
+                {
+                    FeedbackLbl = "Error encountered with Order ID or input field";
+                }
+            }
+            else
+            {
+                FeedbackLbl = "Order ID field is empty";
+            }
+        }
+
+        // DELETE ORDER
+        public void DeleteOrder(string query, int orderId)
+        {
+            try
+            {
+                conn = new SqlConnection(ConnString);
+                adapter = new SqlDataAdapter();
+                conn.Open();
+
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@orderId", orderId);
+
+                adapter.DeleteCommand = cmd;
+
+                int countDeleted = adapter.DeleteCommand.ExecuteNonQuery();
+
+                if (countDeleted > 0)
+                {
+                    FeedbackLbl = $"Successfully deleted Order {orderId}";
+                }
+                else
+                {
+                    FeedbackLbl = $"Failed to delete Order {orderId}";
+                }
+            }
+            catch (SqlException ex)
+            {
+                FeedbackLbl = ex.ToString();
+            }
+            catch (Exception e)
+            {
+                FeedbackLbl = e.ToString();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        // VALIDATE ORDER ID
+        public Boolean IsValidId()
+        {
+            return !string.IsNullOrEmpty(OrderIdTB2.ToString());
+        }
+    
+    }
+}
